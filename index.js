b56542d10c88156eb3d24b7e8e2640699aab434e
Eugen Illinskih <eilinskih92@gmail.com>
2021-09-20 10:50:10 +0300

    17_09

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const photoArr = [];
-const modalArr = [];
+let photoArr = [];
+let modalArr = [];
 let modalId = null;
 const baseUrl = "https://boiling-refuge-66454.herokuapp.com/images";
-const photoContainer = document.getElementsByClassName("photo-container")[0];
-const modalWindow = document.getElementsByClassName("modal-wrapper")[0];
-const closeBtn = document.getElementsByClassName("close")[0];
-const coments = document.getElementsByClassName("comments")[0];
+const photoContainer = document.getElementById("photo-container");
+const modalWindow = document.getElementById("modal-wrapper");
+const closeBtn = document.getElementById("close");
+const coments = document.getElementById("comments");
+const btn = document.getElementById("btn");
 
 async function getPhotoFlow() {
-    let response = await fetch(baseUrl);
+    const response = await fetch(baseUrl);
     photoArr = await response.json();
     fillPhotoArr();
 };
@@ -16,52 +17,62 @@ async function getPhotoFlow() {
 async function onPhotoClick(e) {
     const modalImg = document.getElementsByClassName("modal-img")[0];
     modalWindow.style.display = "flex";
-    let response = await fetch(`${baseUrl}/${e.target.id}`);
-    let modalData = await response.json();
-    modalId = modalData.id
+    const response = await fetch(`${baseUrl}/${e.target.id}`);
+    const modalData = await response.json();
+    modalId = modalData.id;
     modalImg.src = modalData.url;
-    modalArr = modalData.comments
-    modalArr.map((item) => {
-        const coment = document.createElement("div");
-        const comentDate = document.createElement("span");
-        const comentText = document.createElement("span");
-        comentDate.id = `${item.id}date`;
-        comentText.id = `${item.id}text`;
+    modalArr = modalData.comments;
+    modalArr.forEach((item) => {
+        const coment = createElemWithId("div", item.id);
+        const comentDate = createElemWithId("span", `${item.id}date`);
+        const comentText = createElemWithId("span", `${item.id}text`);
         const comentDay = new Date(item.date);
-        let date = (comentDay.getDate() < 10) ? "0" + comentDay.getDate() : comentDay.getDate();
-        let month = ((comentDay.getMonth() + 1) < 10) ? "0" + (comentDay.getMonth() + 1) : (comentDay.getMonth() + 1);
+        const date = (comentDay.getDate() < 10) ? "0" + comentDay.getDate() : comentDay.getDate();
+        const month = ((comentDay.getMonth() + 1) < 10) ? "0" + (comentDay.getMonth() + 1) : (comentDay.getMonth() + 1);
         coments.append(coment);
         coment.classList.add("coment");
-        coment.id = item.id;
         coment.append(comentDate);
         document.getElementById(`${item.id}date`).innerHTML = `${date}.${month}.${comentDay.getFullYear()}`;
-        document.getElementById(`${item.id}date`).style.color = "#CCCCCC"
+        document.getElementById(`${item.id}date`).style.color = "#CCCCCC";
         coment.append(comentText);
         document.getElementById(`${item.id}text`).innerHTML = item.text;
     })
 };
 
-async function SendComent() {
+async function sendComent() {
     const name = document.getElementById("name");
     const text = document.getElementById("text");
-    let response = fetch(`${baseUrl}/${modalId}/comments`, {
+    const comentBody = {name: name.value, comment: text.value};
+    const response = await fetch(`${baseUrl}/${modalId}/comments`, {
         method: 'POST',
-
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(comentBody) 
     });
-}
+    if (response.ok) {
+       alert("succesfully sended") 
+    }
+};
+
+function createElemWithId(el, id) {
+    const elem = document.createElement(el);
+    elem.setAttribute('id', id);
+    return elem
+    };
 
 function onCloseClick() {
     const comentsContainer = document.getElementsByClassName('coment');
-    comentsArr = [];
+    modalId = null;
     for (let i = 0; i < comentsContainer.length; i++) {
         let coment = document.getElementById(comentsContainer[i].id);
-        coment.remove()
+        coment.remove();
     }
     modalWindow.style.display = "none";
 };
 
 function fillPhotoArr() {
-    photoArr.map(item => {
+    photoArr.forEach(item => {
         let photo = document.createElement("img");
         photo.id = item.id;
         photoContainer.append(photo);
@@ -69,9 +80,9 @@ function fillPhotoArr() {
     })
 };
 
-
 getPhotoFlow();
 photoContainer.addEventListener("click", onPhotoClick);
 closeBtn.addEventListener("click", onCloseClick);
+btn.addEventListener("click", sendComent);
 
 
